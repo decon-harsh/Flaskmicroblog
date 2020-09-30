888a527e930cd97447e1a726fc15f675501db010
Abhijit Anand <55055946+Abhijit5463@users.noreply.github.com>
2020-09-30 23:36:41 +0530

    Update models.py

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -25,6 +25,7 @@ class User(db.Model,UserMixin):
 class Post(db.Model):
     id=db.Column(db.Integer,primary_key=True)
     title=db.Column(db.String(100),nullable=False)
+    likes=db.Column(db.Integer, default =0)
     date_posted =db.Column(db.DateTime,nullable=False,default=datetime.utcnow)
     content=db.Column(db.Text,nullable=False)
     user_id=db.Column(db.Integer,db.ForeignKey('user.id'),nullable=False)
